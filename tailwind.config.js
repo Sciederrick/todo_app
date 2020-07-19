2de4be5d1345c395ec80257342cc3041d912ecca
Sciederrick <derrickmbarani@gmail.com>
2020-07-19 16:03:13 +0300

    purged unused tailwind css styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/App.vue',
+    './src/components/addTask.vue',
+    './src/components/blankTask.vue',
+    './src/components/header.vue',
+    './src/components/footer.vue',
+    './src/components/myTodos.vue'
+  ],
   theme: {
     extend: {},
   },
