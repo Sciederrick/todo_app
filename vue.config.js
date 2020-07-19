4a53a1a5bbe59d24c44eb9ff97f09caef6ad89c5
Sciederrick <derrickmbarani@gmail.com>
2020-07-19 18:48:17 +0300

    added a proxy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
 module.exports = {
+  devServer: {
+    proxy: {
+      '/todos': {
+        target: 'http://localhost:5000'
+      }
+    }
+  },
   outputDir: path.resolve(__dirname, './server/public')
 }
