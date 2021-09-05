861e985bf12780d585c14f103c087a1fc31ab34d
Derrick Mbarani <derrickmbarani@gmail.com>
2021-09-05 16:11:07 +0300

    changed to roboto font

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@
   </div>
 </template>
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap');
 
 #app {
   font-family: 'Roboto', sans-serif;
